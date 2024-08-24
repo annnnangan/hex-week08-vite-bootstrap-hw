69b85f167a6f558b1a3b2aed49a007c65e3242ee
annnnangan <anna.ymngan@gmail.com>
2024-08-24 23:19:19 +0800

    Set up Banner

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log("Hello world!");
+var bannerSwiper = new Swiper(".bannerSwiper", {
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 1,
+  slidesPerView: 1.2,
+  grabCursor: true,
+  rewind: true,
+  loop: true,
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-blog-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
